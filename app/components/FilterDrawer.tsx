cd8780280cdfe988658d887ae8b2d3c1dd1f171e
Martin Zejda <martin.zejda@bistudio.com>
2024-07-17 14:58:10 +0200

    Svg icons optimalization

--- app/components/FilterDrawer.tsx
+++ app/components/FilterDrawer.tsx
@@ -35,20 +35,7 @@ export default function FilterDrawer({
               setShowFilter(!showFilter);
             }}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={3}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18 18 6M6 6l12 12"
-              />
-            </svg>
+            <img width={24} src="/public/icons/xMark.svg" />
           </button>
         </div>
 
@@ -108,20 +95,7 @@ function categoryCheckbox(category: string) {
           id="check"
         />
         <span className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-3.5 w-3.5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            stroke="currentColor"
-            strokeWidth="1"
-          >
-            <path
-              fillRule="evenodd"
-              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
+          <img width={16} src="/public/icons/check.svg" />
         </span>
       </label>
       <label
@@ -147,20 +121,7 @@ function priceRangeCheckbox(priceRange: PriceRange) {
           id="check"
         />
         <span className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-3.5 w-3.5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            stroke="currentColor"
-            strokeWidth="1"
-          >
-            <path
-              fillRule="evenodd"
-              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
+          <img width={16} src="/public/icons/check.svg" />
         </span>
       </label>
       <label
--- app/components/ProductList.tsx
+++ app/components/ProductList.tsx
@@ -36,20 +36,7 @@ export default function ProductList({
         </div>
 
         <button onClick={() => setShowFilter(!showFilter)}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z"
-            />
-          </svg>
+          <img width={24} src="/icons/funnel.svg" />
         </button>
       </div>
 
@@ -71,39 +58,13 @@ export default function ProductList({
         className="w-full flex justify-center 
                 items-center gap-4"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="text-separator size-4"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 19.5 8.25 12l7.5-7.5"
-          />
-        </svg>
+        <img width={16} src="/icons/chevronLeft.svg" />
 
         <p className="text-base font-semibold text-fontPrimary">1</p>
         <p className="text-base font-normal text-fontSecondary">2</p>
         <p className="text-base font-normal text-fontSecondary">3</p>
 
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="size-4"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="m8.25 4.5 7.5 7.5-7.5 7.5"
-          />
-        </svg>
+        <img width={16} src="/icons/chevronRight.svg" />
       </div>
     </div>
   );
