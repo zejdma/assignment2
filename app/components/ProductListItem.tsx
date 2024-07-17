f9ea2d7878c02159454d112dd5a14cccda4ecbbc
Martin Zejda <martin.zejda@bistudio.com>
2024-07-17 11:06:21 +0200

    ProductList Item bestSeller badge add to cart button

--- app/components/ProductListItem.tsx
+++ app/components/ProductListItem.tsx
@@ -1,10 +1,23 @@
 import { Product } from "~/types/product";
+import Button from "./Button";
+import { ButtonVariant } from "~/enums/buttonVariant";
 
 export default function ProductListItem({ product }: { product: Product }) {
   return (
     <div className="space-y-2">
-      {productImage(product)}
+      <div className="relative">
+        {productImage(product)}
 
+        {bestSellerBadge(product.bestseller)}
+
+        <div className="absolute bottom-0 w-full">
+          <Button
+            variant={ButtonVariant.primary}
+            title="ADD TO CART"
+            onClick={() => console.log("Click on add to cart")}
+          />
+        </div>
+      </div>
       <div className="flex justify-between items-center">
         <div>
           <p className="text-2xl font-bold text-fontPrimary">{product.name}</p>
@@ -37,6 +50,16 @@ function productImage(product: Product) {
         src={product.image.src}
         alt={product.image.alt}
       ></img>
+    );
+  }
+}
+
+function bestSellerBadge(isBestSeller: boolean) {
+  if (isBestSeller) {
+    return (
+      <p className="absolute top-0 left-0 text-fontPrimary text-sm md:text-sm font-bold py-2 px-4 bg-background">
+        Best Seller
+      </p>
     );
   }
 }
